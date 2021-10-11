7b7f5547e40cebbbcdc64c72c099947997aef07f
Toby Travis <tobytravis@tobys-mbp.home>
2021-10-11 21:21:15 +0100

    Testing function on netlify

--- components/VoterList.vue
+++ components/VoterList.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
     <h2>Voter List</h2>
-    <div v-for="user in users" :key="user.ref">
-      {{ user.firstName }}
+    <div v-for="voter in voters" :key="voter.ref">
+      {{ voter.firstName }}
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    voters: {
-      type: Array,
-      default() {
-        return []
-      },
-    },
+  data() {
+    return {
+      voters: [],
+    }
   },
+  async fetch() {
+    const hello = await this.$axios.get('/.netlify/functions/hello')
+    console.log(hello)
+    // const { voters } = await this.$axios.get('/.netlify/functions/fetch_voters')
+    // this.voters = voters
+  },
+  fetchOnServer: false,
 }
 </script>
 
---
+++ functions/fetch-voters.js
@@ -0,0 +1,22 @@
+// Credit to Josh Comeau 
+// const faunadb = require('faunadb');
+exports.handler = async (event) => {
+    // console.log('here')
+    // const q = faunadb.query;
+    // const client = new faunadb.Client({
+    //     secret: process.env.FAUNA_SECRET_KEY,
+    // })
+    // const document = await client.query(
+    //     q.Get(q.Index('all_customers'))
+    // )
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Hello World" })
+    };
+    // return {
+    //     statusCode: 200,
+    //     body: JSON.stringify({
+    //         likes: document.data,
+    //     }),
+    // };
+};
